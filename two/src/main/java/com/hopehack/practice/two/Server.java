6559198adec25c2c4a2c69edd34c74d1b42933e4
dongchao <hdbysj@163.com>
2022-10-15 23:05:40 +0800

    Reuse ByteBuffer for read data from socket channel and write data to file channel

--- two/src/main/java/com/hopehack/practice/two/Server.java
+++ two/src/main/java/com/hopehack/practice/two/Server.java
@@ -17,19 +17,27 @@ import java.util.Set;
  */
 public class Server {
 
-
     public static void start() throws IOException {
+        // 开启Selector
         Selector selector = Selector.open();
+        // 开启一个Server Socket Channel 用于监听连接Accept事件
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        // 设置成非阻塞模式
         serverSocketChannel.configureBlocking(false);
+        // 监听端口
         serverSocketChannel.bind(new InetSocketAddress(20023));
+        // 设置Selector 多路复用监听Accept事件
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-        long currentTimeMillis = System.currentTimeMillis();
-        String localFile =  "/Users/dongchao/" + currentTimeMillis;
+        // 产生一个文件名
+        String localFile = getFileName();
+        // 获取一个File Channel
         FileOutputStream fos = new FileOutputStream(localFile);
         FileChannel outFileChannel = fos.getChannel();
+        // 记录文件字节量
         long outLength = 0;
+        ByteBuffer byteBuffer = ByteBuffer.allocate(204800);
+
+        // 阻塞选择准备好进行I/O操作的键
         while(selector.select() > 0) {
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
@@ -40,15 +48,17 @@ public class Server {
                     SocketChannel socketChannel = serverSocketChannel1.accept();
                     socketChannel.configureBlocking(false);
                     socketChannel.register(selector, SelectionKey.OP_READ);
-
                 } else if (selectionKey.isReadable()) {
                     SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-                    int length = 0;
-                    ByteBuffer byteBuffer = ByteBuffer.allocate(204800);
+                    int length;
+                    // 对于Socket Channel来说是从Channel上读取数据，写入到ByteBuffer
                     while ((length = socketChannel.read(byteBuffer)) > 0) {
+                        // 切换成读模式
                         byteBuffer.flip();
+                        // 对于File Channel来说是读取ByteBuffer数据，写入到Channel
                         outFileChannel.write(byteBuffer);
                         outLength = outLength + length;
+                        // 切换成写模式
                         byteBuffer.clear();
                         outFileChannel.force(true);
                     }
@@ -63,6 +73,11 @@ public class Server {
         }
         fos.close();
         outFileChannel.close();
+    }
+
+    private static String getFileName() {
+        long currentTimeMillis = System.currentTimeMillis();
+        return "/Users/dongchao/" + currentTimeMillis;
     }
 
     public static void main(String[] args) throws IOException {
